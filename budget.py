02514f4e717d4513ce0c5f3329d385b28b13e190
remchaf <70279203+remchaf@users.noreply.github.com>
2022-07-24 23:26:31 +0000

    Writing the create_spend_chart function within budget.py

--- budget.py
+++ budget.py
@@ -1,53 +1,126 @@
+from functools import total_ordering
+from tkinter.tix import AUTO
+
+
 class Category:
-    ledger = list()
+    ledger = None
     name = ''
-    
+
     def __init__(self, name_arg) -> None:
         self.name = name_arg
-        print(name_arg, ' was created !')
-        
-    
+        self.ledger = list()
+
     def __str__(self) -> str:
         stars_number = (30 - len(self.name)) // 2
         string = '*' * stars_number + self.name + '*' * stars_number + '\n'
-        
-        for item in self.ledger :
+
+        for item in self.ledger:
             (desc, amt) = (item['description'][:23], str(item['amount'])[:7])
-            string += desc + ' ' * ( 30 - len(desc) - len(amt)  ) + amt + '\n'
-            
-        string += 'Total: ' + self.get_balance()
-        
+            string += desc + ' ' * (30 - len(desc) - len(amt)) + amt + '\n'
+
+        string += 'Total: ' + str(self.get_balance())
+
         return string
-        
-        
-    def deposit(self, amount, description='') :
-        self.ledger.append({ amount: float(amount), description: description })
+
+    def deposit(self, amount, description=''):
+        self.ledger.append(
+            {'amount': float(amount), 'description': description})
         return None
-    
-    def withdraw(self, amount, description='') :
-        if self.check_funds(amount) :
-            self.ledger.append({ amount: -float(amount), description: description })
+
+    def withdraw(self, amount, description=''):
+        if self.check_funds(amount):
+            self.ledger.append(
+                {'amount': -float(amount), 'description': description})
             return True
         return False
-    
-    def get_balance(self) :
+
+    def get_balance(self):
         balance = 0
-        for ldger in self.ledger :
+        for ldger in self.ledger:
             balance += ldger['amount']
         return balance
-    
-    def transfer(self, amount, budget_category) :
-        if self.check_funds(amount) :
-            self.withdraw(amount, 'Transfer to', budget_category.name)
-            budget_category.deposit(amount, 'Transfer from', self.name)
+
+    def transfer(self, amount, budget_category):
+        if self.check_funds(amount):
+            self.withdraw(amount, ('Transfer to ' + budget_category.name))
+            budget_category.deposit(amount, 'Transfer from ' + self.name)
             return True
         return False
-    
-    def check_funds(self, amount) :
+
+    def check_funds(self, amount):
         return self.get_balance() > amount
 
 
-
-
 def create_spend_chart(categories):
-    return
+
+    def helper(cat):
+        return len(cat)
+
+    perc = dict()
+    string = 'Percentage spent by category \n'
+    label = [((3 - len(str(x))) * " " + str(x) + '|')
+             for x in range(100, -1, -10)]
+
+    for categorie in categories:
+        total_withdraws = 0
+        for entry in categorie.ledger:
+            if entry['amount'] < 0:
+                total_withdraws += entry['amount']
+
+        perc[categorie.name] = abs(total_withdraws)
+    total = sum(perc.values())
+
+    for key in perc:
+        key_percentage = int(perc[key] / total * 100)
+        key_arr = []
+        for i in range(100, -1, -10):
+            if key_percentage >= i:
+                key_arr.append('o')
+            else:
+                key_arr.append('')
+
+        perc[key] = key_arr
+
+    for i in range(11):
+        string += label[i] + ' '
+        for key in perc:
+            string += perc[key][i] + '  '
+        string = string.rstrip() + '\n'
+
+    string += '    ' + '-' * (len(categories) * 3 + 1) + '\n'
+
+    longest_name = max(perc, key=helper)
+    for i in range(len(longest_name)) :
+        string += '     '
+        for cat in perc :
+            try :
+                string += cat[i:i+1][0] + '  '
+            except :
+                string += '   '
+        string = string.rstrip() + '\n'
+        
+    
+
+    return string
+
+
+Food = Category('Food')
+Food.deposit(25000, 'Salary of the week')
+Food.withdraw(650, 'Sugar')
+Food.withdraw(2000, 'Credit telephonique')
+# print(Food)
+
+Trans = Category('Transport')
+Trans.deposit(2000, 'this is it !')
+Trans.withdraw(1500, 'One way , to work')
+# print(Trans)
+
+Auto = Category('Auto')
+Auto.deposit(125630, 'First deposit')
+Auto.withdraw(3410)
+
+# Food.transfer(25000, Trans)
+
+# print(Food, Trans, sep='\n')
+
+print(create_spend_chart([Food, Trans, Auto]))
